0ff9b23d1cd2642725c86aa0212d6078764abc9a
diego <diegochueri@gmail.com>
2023-07-22 16:57:16 -0300

    feat: add database

---
+++ src/database/orm-config.ts
@@ -0,0 +1,14 @@
+const config = () => {
+  return {
+    type: process.env.DB_TYPE as any,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities: ["dist/**/*.entity.js"],
+    synchronize: true
+  }
+}
+
+export default config
--- src/user/users.service.ts
+++ src/user/users.service.ts
@@ -1,33 +1,54 @@
-//@ts-nocheck
 import { Injectable } from '@nestjs/common';
 import { UserCreateDTO } from './dto/user-create.dto';
 import { UserUpdateDto } from './dto/user-update.dto';
 import { IUser } from './user.interface';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/users.entity';
+import * as dayjs from 'dayjs';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) { }
 
   async getAll(): Promise<IUser[]> {
-    return 'users' 
+    return await this.usersRepository.find({
+      where: {
+        deletedAt: null,
+      }
+    })
   }
 
   async getOneById(id: string): Promise<IUser> {
-    return 'userById' + id
+    return await this.usersRepository.findOneBy({ id, deletedAt: null })
   }
 
   async getOneByEmail(email: string): Promise<IUser> {
-    return 'userByEmail' + email
+    return await this.usersRepository.findOneBy({ email, deletedAt: null })
   }
 
   async create(createUserDto: UserCreateDTO): Promise<IUser> {
-    return 'createdUser'
+    const user = this.usersRepository.create(createUserDto)
+    return await this.usersRepository.save(user)
   }
 
   async update(id: string, updateUserDto: UserUpdateDto): Promise<IUser> {
-    return 'updatedUser'
+    const updateResult = await this.usersRepository
+      .createQueryBuilder()
+      .update(id, updateUserDto)
+      .where({ deletedAt: null })
+      .execute()
+    return updateResult.raw;
   }
 
   async delete(id: string): Promise<void> {
-
+    await this.usersRepository
+      .createQueryBuilder()
+      .update(id, { deletedAt: dayjs().toISOString() })
+      .where({ deletedAt: null })
+      .execute()
   }
 }
